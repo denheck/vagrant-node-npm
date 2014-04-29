caa2af587d2c0a1c0a7f14d11300d9ed1c8f5398
Dennis Heckman <dennis.heckman@vertive.com>
2014-04-29 12:04:10 -0500

    add Vagrantfile and provisioner

---
+++ provision.sh
@@ -0,0 +1,5 @@
+apt-get update
+apt-get install software-properties-common python-software-properties --yes
+add-apt-repository ppa:chris-lea/node.js
+apt-get update
+apt-get install python g++ make nodejs --yes
